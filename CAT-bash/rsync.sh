b09013fa486720c7e4d8a63fdbbbf963a0b2a4e1
Simon Levesque <simon@foilen.com>
2019-10-14 18:53:55 -0400

    rsync - Add local

--- CAT-bash/rsync.sh
+++ CAT-bash/rsync.sh
@@ -1,4 +1,12 @@
-# Synchronise by SSH, updating files all at the end once all transfered
+# Synchronise, updating all files at the end
+rsync --delay-updates --delete -zrtv SOURCE_FILE_OR_DIRECTORY TARGET_DIRECTORY
+rsync --delay-updates --delete -zrtv /etc/apache2/ /backup/etc/apache2
+
+# Synchronise, updating files right away
+rsync --inplace --delete -zrtv SOURCE_FILE_OR_DIRECTORY TARGET_DIRECTORY
+rsync --inplace --delete -zrtv /etc/apache2/ /backup/etc/apache2
+
+# Synchronise by SSH, updating all files at the end once all transfered
 # Note that you can switch the local and remote parts to copy the other way
 rsync --delay-updates --compress-level=9 --delete -zrtve "SSH_PARAMS" HOST_OR_IP:REMOTE_FILE_OR_DIRECTORY LOCAL_DIRECTORY
 rsync --delay-updates --compress-level=9 --delete -zrtve "ssh -i /home/user/private.pem -l ec2-user" 192.168.1.1:/etc/apache2/ /backup/etc/apache2
